fdb212b7976e2497625ec0ade36f8f23c7a69286
Omar Hernández <127421759+omar15hr@users.noreply.github.com>
2025-05-29 10:06:57 -0400

    imagen hero

--- src/components/home-page/HeroSection.tsx
+++ src/components/home-page/HeroSection.tsx
@@ -1,14 +1,17 @@
+import Image from "next/image";
+import heroImage from "../../../public/img/img-header.webp";
+
 export function HeroSection() {
   return (
-    <section className="r elative h-[600px] overflow-hidden p-5">
+    <section className="relative h-[600px] overflow-hidden p-5">
       <div className="absolute inset-0 bg-gradient-to-r from-white via-white/80 to-transparent z-10"></div>
-      {/* <Image
-    width={1920}
-    height={1080}
-    src={heroImage}
-    alt="Sports apparel collection"
-    className="absolute inset-0 w-full h-full object-cover object-top"
-  /> */}
+      <Image
+        width={1920}
+        height={1080}
+        src={heroImage}
+        alt="Sports apparel collection"
+        className="absolute inset-0 w-full h-full object-cover"
+      />
       <div className="relative z-20 container mx-auto px-4 h-full flex items-center animate-fadeIn">
         <div className="max-w-lg">
           <h2 className="text-5xl font-bold text-gray-900 mb-4">
